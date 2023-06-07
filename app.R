0fdbb28b0db99825629cfa560f6950b58d581289
howardbaek <howardbaek.fh@gmail.com>
2023-06-07 15:12:36 -0700

    Reactives work

--- app.R
+++ app.R
@@ -1,29 +1,16 @@
 library(shiny)
 library(ari)
 
-## Proof of concept script
-
-# gs_url <- "https://docs.google.com/presentation/d/1sFsRXfK7LKxFm-ydib5dxyQU9fYujb95katPRu0WVZk/edit#slide=id.p"
-# 
-# # Script (text)
-# pptx_path <- download_gs_file(gs_url, out_type = "pptx")
-# pptx_notes_vector <- pptx_notes(pptx_path)
-# 
-# # Images
-# pdf_path <- download_gs_file(gs_url, out_type = "pdf")
-# filenames <- pdf_to_pngs(pdf_path)
-# 
-# # Create a video from images and text
-# ari_spin(images = filenames, paragraphs = pptx_notes_vector, service = "coqui")
-
-
 ui <- fluidPage(
   titlePanel("Loqui: Make videos"),
   
   sidebarLayout(
     sidebarPanel(
-      textInput("gs_url", 
-                label = "Google Slides URL")	
+      textInput("url", 
+                label = "Google Slides URL",
+                value = "",
+                placeholder = "URL"),
+      actionButton("go", "Generate")
     ),
     mainPanel(h4("Rendered Video (mp4)"),
               uiOutput("video"))
@@ -31,13 +18,32 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  output$video <- renderUI({
-    tags$video(src = "sample.mp4", 
-               type = "video/mp4",
-               height ="400px", 
-               width="400px",
-               autoplay = TRUE, 
-               controls = TRUE)
+  
+  observeEvent(input$go, {
+    # text
+    pptx_path <- download_gs_file(input$url, out_type = "pptx")
+    pptx_notes_vector <- pptx_notes(pptx_path)
+    
+    # images
+    pdf_path <- download_gs_file(input$url, out_type = "pdf")
+    image_path <- pdf_to_pngs(pdf_path)
+    
+    # create video
+    ari_spin(images = image_path, 
+             paragraphs = pptx_notes_vector,
+             service = "coqui",
+             output = "www/ari-video.mp4")
+    
+    output$video <- renderUI({
+      tags$video(src = "ari-video.mp4", 
+                 type = "video/mp4",
+                 height ="400px", 
+                 width="400px",
+                 autoplay = TRUE, 
+                 controls = TRUE)
+    })
+    
+    
   })
 }
 
